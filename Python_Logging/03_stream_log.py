5cdb2197dc2eed1de2011e97351546e440ae2dd3
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-15 17:14:22 +0800

    Delete Python_Logging directory

--- Python_Logging/03_stream_log.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logger = logging.getLogger('stream_logger')
-logger.setLevel(logging.INFO)
-
-console = logging.StreamHandler()
-
-logger.addHandler(console)
-logger.info("This is an informational message")
--- Python_Logging/07_logging_with_config_file.py
+++
@@ -1,8 +0,0 @@
-import logging
-import logging.config
-
-logging.config.fileConfig('logging.conf')
-logger = logging.getLogger("exampleApp")
-
-logger.info("Program started")
-logger.info("Done!")
